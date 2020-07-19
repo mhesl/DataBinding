f896bc7d2b05c78e2f536ffccfddd87b4cd7d7b5
mohamadeslami <mohammadeslami1379@yahoo.com>
2020-07-19 16:37:40 +0430

    navigation functionality completed

--- app/src/main/java/com/example/databinding/AddNoteFragment.kt
+++ app/src/main/java/com/example/databinding/AddNoteFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_add_note.*
 
 
 class AddNoteFragment : Fragment() {
@@ -14,6 +16,15 @@ class AddNoteFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_note, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        buttonSave.setOnClickListener {
+            val action =  AddNoteFragmentDirections.actionSaveNote()
+            Navigation.findNavController(it).navigate(action)
+
+        }
     }
 
 
--- app/src/main/java/com/example/databinding/HomeFragment.kt
+++ app/src/main/java/com/example/databinding/HomeFragment.kt
@@ -5,14 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +20,12 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        buttonAdd.setOnClickListener {
+            val action : NavDirections = HomeFragmentDirections.actionAddNote()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
