c2e0d1659353ce8ab26d145d873b3c14d9c3495e
mohamadeslami <mohammadeslami1379@yahoo.com>
2020-07-21 00:23:17 +0430

    AddNoteFragment completed for saving one note in database

--- app/src/main/java/com/example/databinding/AddNoteFragment.kt
+++ app/src/main/java/com/example/databinding/AddNoteFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.example.databinding.room.Note
+import com.example.databinding.room.NoteDataBase
 import kotlinx.android.synthetic.main.fragment_add_note.*
+import kotlinx.android.synthetic.main.item_note.*
+import kotlinx.coroutines.launch
 
 
-class AddNoteFragment : Fragment() {
+class AddNoteFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,10 +25,37 @@ class AddNoteFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         buttonSave.setOnClickListener {
-            val action =  AddNoteFragmentDirections.actionSaveNote()
-            Navigation.findNavController(it).navigate(action)
+            val noteTitle:String = titleET.text.toString().trim()
+            val noteBody :String = bodyET.text.toString().trim()
 
+            if(noteTitle.isNullOrEmpty()){
+                titleET.error = "Title error"
+                return@setOnClickListener
+            }
+            if(noteBody.isNullOrEmpty()){
+                titleET.error = "body error"
+                return@setOnClickListener
+            }
+            val newNote =Note(noteTitle , noteBody)
+
+            launch {
+                saveNote(newNote)
+            }
+
+
+
+            navigateBack()
         }
+    }
+
+    private suspend fun saveNote(note:Note){
+            context?.let {
+                NoteDataBase(it).getNodeDao().addNote(note)
+            }
+    }
+    private fun navigateBack(){
+        val action =  AddNoteFragmentDirections.actionSaveNote()
+        Navigation.findNavController(buttonSave).navigate(action)
     }
 
 
