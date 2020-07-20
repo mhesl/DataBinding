0b8a885f3b0fc50582dbdee614d8309988f45ef8
mohamadeslami <mohammadeslami1379@yahoo.com>
2020-07-21 00:56:21 +0430

    reading from database and showing notes in recyclerview completed

--- app/src/main/java/com/example/databinding/HomeFragment.kt
+++ app/src/main/java/com/example/databinding/HomeFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.databinding.room.Note
+import com.example.databinding.room.NoteDataBase
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.launch
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +31,16 @@ class HomeFragment : Fragment() {
         buttonAdd.setOnClickListener {
             val action : NavDirections = HomeFragmentDirections.actionAddNote()
             Navigation.findNavController(it).navigate(action)
+        }
+        notesRV.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+        }
+        launch {
+            context?.let {
+                val notes : List<Note> = NoteDataBase(it).getNodeDao().getAllNotes()
+                notesRV.adapter = NotesAdapter(notes)
+            }
         }
     }
 }
